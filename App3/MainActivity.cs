d3759f0ffdb544040cc4d2feb93a8017e7b1c547
ninjavaz <d1n22@onet.pl>
2020-09-09 21:46:37 +0200

    This is a new commit for what I originally planned to be an amendmend

---
+++ App3/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace App3
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        int how = 0;
+        
+       
+        
+        
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            //My code:
+            
+            var TextView1 = FindViewById<TextView>(Resource.Id.textView1);
+            var button1 = FindViewById<Button>(Resource.Id.button1);
+            
+            void Dodaj()
+            {
+                how += 1;
+                TextView1.Text = "" + how;
+            }
+
+
+
+            button1.Click += (e, a) =>
+            {
+                Dodaj();
+            };
+        
+        
+        }
+        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
+    }
+}
